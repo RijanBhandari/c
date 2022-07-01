65dc7e50634854f5aed1337456a88b2188e605cf
Rijan Bhandari <102028111+RijanBhandari@users.noreply.github.com>
2022-07-01 14:45:55 +0545

    Add files via upload

---
+++ calc_area_and_circum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void calc(int,float*,float*);
+#define pi 3.14
+void main(){
+int r;
+float A,C;
+printf("Enter radius of circle");
+scanf("%d",&r);
+calc(r,&A,&C);
+printf("The area is %f\n",A);
+printf("The circumference is %f",C);
+}
+
+void calc(int r,float *A,float *C){
+    *A = pi * r * r;
+    *C = 2 * pi * r;
+}
